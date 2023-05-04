a27973526a09d9e0214f9f54c60e1583ac9e672a
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-04 02:44:30 +0200

    Style: change siv spinner to white

--- src/pages/Loader.tsx
+++ src/pages/Loader.tsx
@@ -5,7 +5,7 @@ type Props = {}
 const Loader = (props: Props) => {
   return (
     <div className="flex items-center justify-center">
-  <div className="w-12 h-12 border-t-4 border-b-4 border-purple-700 rounded-full animate-spin"></div>
+  <div className="w-12 h-12 border-t-4 border-b-4 border-white rounded-full animate-spin"></div>
 </div>
 
   )
