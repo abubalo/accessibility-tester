8230af718921e0a1f91268be2dc3cdf98f7c6e71
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-04 02:57:23 +0200

    Refactored: remove console log

--- src/pages/api/server.ts
+++ src/pages/api/server.ts
@@ -1,28 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import pa11y from "pa11y";
 
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { url } = req.body;
-  console.log("URL: ", req.body);
+      const { url } = req.body;
 
-  // Check if the URL is valid
-  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
-  if (!urlRegex.test(url)) {
-    return res.status(400).json({ error: "Invalid URL format" });
-  }
+      // Check if the URL is valid
+      const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+      if (!urlRegex.test(url)) {
+        return res.status(400).json({ error: "Invalid URL format" });
+      }
 
-  if (!url || typeof url !== "string" || url === "") {
-    return res.status(400).json({ error: "Invalid url" });
-  }
+      if (!url || typeof url !== "string" || url === "") {
+        return res.status(400).json({ error: "Invalid url" });
+      }
 
-  try {
-    const result = await pa11y(url);
-    res.json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to perform accessibility testing" });
-  }
+
+      try {
+        const result = await pa11y(url);
+        res.json(result);
+      } catch (error) {
+        res.status(500).json({ error: "Failed to perform accessibility testing" });
+      }
 }
